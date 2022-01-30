3de38f100a3a2032ada1fe38f7559ca6d876efbe
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-01-30 21:28:12 +0100

    Change function titleClickHandler (get 'href' attribute from the clicked link, find the correct article using the selector, add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+{
+
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event)
+  console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -25,15 +28,25 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector)
 
-  /* add class 'active' to the correct article */
+  /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector('href');
+  console.log(clickedElement.getAttribute('li'))
+
+  /* [IN PROGRESS] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
+
 }
